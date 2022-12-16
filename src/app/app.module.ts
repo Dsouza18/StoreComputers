2bf0db4b88a252110c21b3be832ccb4389e4c19e
Daniel Santos <daniel.santos1805@hotmail.com>
2022-12-16 10:55:51 -0300

    incluindo snack-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+import ptBr from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+
+import { FooterComponent } from './footer/footer.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSnackBarModule } from "@angular/material/snack-bar"
+
+registerLocaleData(ptBr);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NaoEncontradoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSnackBarModule
   ],
-  providers: [],
+  providers: [ { provide: LOCALE_ID, useValue: 'pt' } ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/produtos/detalhes-produto/detalhes-produto.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NotificacaoService } from 'src/app/notificacao.service';
+import { IProduto } from 'src/app/produtos';
+import { ProdutosService } from 'src/app/produtos.service';
+
+@Component({
+  selector: 'app-detalhes-produto',
+  templateUrl: './detalhes-produto.component.html',
+  styleUrls: ['./detalhes-produto.component.css']
+})
+export class DetalhesProdutoComponent implements OnInit {
+
+    produto: IProduto | undefined;
+    quantidade = 1;
+
+  constructor(
+    private produtosService: ProdutosService,
+    private route: ActivatedRoute,
+    private notificacaoService: NotificacaoService
+  ){}
+
+  ngOnInit(): void {
+    const routeParams = this.route.snapshot.paramMap;
+    const produtoId = Number(routeParams.get("id"));
+    this.produto = this.produtosService.getOne(produtoId);
+  }
+
+  adicionarAoCarrinho(){
+    this.notificacaoService.notificar("O produto foi adicionado ao carrinho")
+  }
+
+}
